fcb0063284801b3a8d23bee70023e7202c4c57ab
Sebastian Feuerstack <Sebastian@Feuerstack.de>
2010-12-14 22:09:22 -0200

    fixed problem with missing state definition in test statemachine

--- spec/sm_parallel_state_spec.rb
+++ spec/sm_parallel_state_spec.rb
@@ -1,9 +1,7 @@
 require File.dirname(__FILE__) + '/spec_helper'
 
-describe "Turnsule nested parallel" do
-
+describe "Nested parallel" do
   before(:each) do
-
     @out_out_order = false
 
     @sm = Statemachine.build do
@@ -13,6 +11,7 @@ describe "Turnsule nested parallel" do
         statemachine :s1 do
           superstate :operative do
             trans :locked, :coin, :unlocked, Proc.new { @locked = false }
+            trans :unlocked, :coin, :locked
             event :maintain, :maintenance, Proc.new { @out_of_order = true }
           end
         end
@@ -32,19 +31,19 @@ describe "Turnsule nested parallel" do
     @sm.state.should eql :start
     @sm.go
     @sm.state.should eql :p
- #   @sm.states.should == [:locked,:on]
+    @sm.states.should == [:locked,:on]
     @sm.coin
     @sm.state.should eql :p
-  #  @sm.states.should == [:unlocked,:on]
+    @sm.states.should == [:unlocked,:on]
     @sm.toggle
     @sm.state.should eql :p
-   # @sm.states.should == [:unlocked,:off]
+    @sm.states.should == [:unlocked,:off]
   end
 
   it "supports leaving a parallel state" do
-    #@sm.states.should == [:start]
+    @sm.states.should == [:start]
     @sm.go
-#    @sm.states.should == [:locked,:on]
+    @sm.states.should == [:locked,:on]
     @sm.maintain
     @sm.state.should == :maintenance
 
@@ -53,20 +52,27 @@ describe "Turnsule nested parallel" do
   it "support testing with 'in' condition for  superstates " do
     @sm.go
     @sm.process_event(:coin)
-    #@sm.is_in_state?(:unlocked).should == true
+    @sm.is_in_state?(:unlocked).should == true
   end
 
-  #it "support testing with 'in' condition for parallel  superstates " do
-  #  @sm.go
-  #  @sm.process_event(:coin)
-    # @sm.is_in_state?(:onoff).should == true TODO tests for superstates not working yet
-    # @sm.is_in_state?(:operative).should == true
-    #@sm.is_in_state?(:on).should == true
+  it "support testing with 'in' condition for parallel  superstates " do
+    @sm.go
+    @sm.coin
+    @sm.is_in_state?(:onoff).should == true
+    @sm.is_in_state?(:operative).should == true
+    @sm.is_in_state?(:on).should == true
 
-    # @sm.is_in_state?(:second).should == true ;; is not supported!!
+#    @sm.is_in_state?(:second).should == true
 
-#    @sm.process_event(:maintain) TODO not working yet!!!
-    #@sm.is_in_state?(:maintenance).should == true
- # end
+    @sm.maintain # TODO not working
+    @sm.is_in_state?(:maintenance).should == true
+  end
 
+  it "supports process_event for parallel states" do
+    @sm.go
+    @sm.process_event(:coin)
+    @sm.is_in_state?(:onoff).should == true
+    @sm.is_in_state?(:operative).should == true
+    @sm.is_in_state?(:on).should == true
+  end
 end
